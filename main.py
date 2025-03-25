25262e7c452eb669dbe0b74da05ff7b987a069af
Denis Humen <dberkut18@gmail.com>
2025-03-25 17:07:49 +0200

    Refactor configuration management and enhance wallet processing logic; remove obsolete config file and add new general config

--- main.py
+++ main.py
@@ -1,3 +1,128 @@
-from modules import monad
+import toml
+import csv
+import random
+import threading
+from tqdm import tqdm
+from questionary import Choice, select
+from colorama import Fore, Style
+import os
+import time
+import requests
+import json
+from config.pyload_presset.random_pyload import random_pyload_presset
+from modules.monad import monad_checker
+import modules.monad as monad
 
-monad.monad_checker()
+config = toml.load('config/general_config.toml')
+num_threads = config.get('THRENDS', 10)
+sleep_between_wallet = config.get('SLEEP_BEATWEEN_WALLET', [1, 3])
+sleep_between_replace_proxy = config.get('SLEEP_BEATWEEN_REAPLECE_PROXY', [1, 3])
+limit_replace_proxy = config.get('LIMIT_REPLACE_PROXY', 10)
+log_file_path = 'results/logs/log'
+
+def log_error(message):
+    with open(log_file_path, 'a') as log_file:
+        log_file.write(message + '\n')
+
+def get_wallets_and_proxies():
+    wallets = []
+    proxies = []
+    with open('data/wallet.csv', mode='r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header
+        for row in reader:
+            if len(row) >= 2:
+                wallets.append(row[0])
+                proxies.append(row[1])
+    return wallets, proxies
+
+def get_reserv_proxies():
+    reserv_proxies = []
+    with open('data/reserv_proxy.csv', mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if len(row) > 0:
+                reserv_proxies.append(row[0])
+    return reserv_proxies
+
+def process_wallets(wallets, proxies, reserv_proxies):
+    results = []
+    threads = []
+    with tqdm(total=len(wallets)) as pbar:
+        for wallet_address, proxy in zip(wallets, proxies):
+            if len(threads) >= num_threads:
+                for t in threads:
+                    t.join()
+                    pbar.update(1)
+                threads = []
+            thread = threading.Thread(target=process_wallet, args=(wallet_address, proxy, reserv_proxies, results))
+            thread.start()
+            threads.append(thread)
+            time.sleep(random.uniform(*sleep_between_wallet))
+        
+        for t in threads:
+            t.join()
+            pbar.update(1)
+    
+    ensure_all_wallets_processed(wallets, proxies, reserv_proxies, results)
+    return results
+
+def process_wallet(wallet_address, proxy, reserv_proxies, results):
+    pyload = random_pyload_presset()
+    success = False
+    attempts = 0
+    while not success and attempts < limit_replace_proxy:
+        try:
+            result = monad_checker(wallet_address, proxy, pyload)
+            results.append(result)
+            success = True
+        except ValueError as e:
+            log_error(str(e))
+            time.sleep(random.uniform(*sleep_between_replace_proxy))
+        except requests.exceptions.ProxyError as e:
+            log_error(f'Proxy error with proxy {proxy}: ' + str(e))
+            time.sleep(random.uniform(*sleep_between_replace_proxy))
+            proxy = random.choice(reserv_proxies)
+        except requests.exceptions.RequestException as e:
+            log_error(f'Request error: ' + str(e))
+            time.sleep(random.uniform(*sleep_between_replace_proxy))
+        except json.JSONDecodeError as e:
+            log_error(f'JSON decode error: ' + str(e))
+            time.sleep(random.uniform(*sleep_between_replace_proxy))
+        attempts += 1
+
+def ensure_all_wallets_processed(wallets, proxies, reserv_proxies, results):
+    for wallet_address in wallets:
+        file_path = f"results/wallet_json_data/{wallet_address}.json"
+        if not os.path.exists(file_path):
+            proxy = random.choice(reserv_proxies)
+            process_wallet(wallet_address, proxy, reserv_proxies, results)
+
+def menu():
+    try:
+        while True:
+            action = select(
+                "What do you want to do?",
+                choices=[
+                    Choice('💲 start stats MONAD', 'stats_monad'),
+                    Choice('❌ Exit', 'exit')
+                ],
+                qmark='🛠️',
+                pointer='👉'
+            ).ask()
+
+            if action == 'exit':
+                break
+            elif action == 'stats_monad':
+                wallets, proxies = get_wallets_and_proxies()
+                reserv_proxies = get_reserv_proxies()
+                results = process_wallets(wallets, proxies, reserv_proxies)
+                monad.process_results(results)
+                
+    except Exception as e:
+        log_error('Error: ' + str(e))
+
+def main():
+    menu()
+
+main()
--- modules/monad.py
+++ modules/monad.py
@@ -1,3 +1,72 @@
-def monad_checker():
-    print("Monad checker")
-    return
+import requests
+from colorama import Fore, Style
+import json
+from config.pyload_presset.random_pyload import random_pyload_presset
+
+log_file_path = 'results/logs/log'
+
+def log_error(message):
+    with open(log_file_path, 'a') as log_file:
+        log_file.write(message + '\n')
+
+def monad_checker(wallet_address, proxy, pyload):
+    if pyload is not None:
+        try:
+            url = f'https://layerhub.xyz/chains/monad_testnet/wallets/{wallet_address}'
+            params = {"_rsc": "1bw6b"}
+            headers = {
+                "accept": "*/*",
+                "accept-language": "ru,en-US;q=0.9,en;q=0.8",
+                "next-router-state-tree": '%5B%22%22%2C%7B%22children%22%3A%5B%22(mainLayout)%22%2C%7B%22children%22%3A%5B%22search%22%2C%7B%22children%22%3A%5B%22__PAGE__%3F%7B%5C%22p%5C%22%3A%5C%22monad_testnet%5C%22%7D%22%2C%7B%7D%2C%22%2Fsearch%3Fp%3Dmonad_testnet%22%2C%22refresh%22%5D%7D%2Cnull%2Cnull%5D%7D%2Cnull%2Cnull%5D%7D%2Cnull%2Cnull%2Ctrue%5D',
+                "next-url": "/search",
+                "rsc": "1",
+                "sec-ch-ua": "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\", \"Google Chrome\";v=\"132\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Linux\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "Referer": "https://layerhub.xyz/search?p=monad_testnet",
+                "Referrer-Policy": "strict-origin-when-cross-origin"
+            }
+
+            response = requests.get(url, params=params, headers=headers, proxies={"http": proxy, "https": proxy})
+            
+            if response.status_code != 200:
+                log_error(f'HTTP error: {response.status_code} - {response.text}')
+                raise ValueError(f'HTTP error: {response.status_code}')
+            
+            if not response.content:
+                log_error('Empty response content')
+                raise ValueError('Empty response content')
+            
+            try:
+                data = response.json()
+                if isinstance(data, dict) and data.get("message") == "Wallet is not found for chain_id: monad_testnet":
+                    log_error("Wallet not found, retrying...")
+                    raise ValueError("Wallet not found, retrying...")
+                data['wallet_address'] = wallet_address
+            except json.JSONDecodeError:
+                data = {"wallet_address": wallet_address, "response": response.text}
+            
+            return data
+        
+        except requests.exceptions.ProxyError as e:
+            log_error('Proxy error: ' + str(e))
+            raise e
+        except requests.exceptions.RequestException as e:
+            log_error('Request error: ' + str(e))
+            raise e
+        except Exception as e:
+            log_error('Error: ' + str(e))
+            raise e
+
+    else:
+        log_error('Error: No pyload found')
+
+def process_results(results):
+    for result in results:
+        wallet_address = result.get('wallet_address', 'unknown_wallet')
+        file_path = f"results/wallet_json_data/{wallet_address}.json"
+        with open(file_path, 'w') as json_file:
+            json.dump(result, json_file)
