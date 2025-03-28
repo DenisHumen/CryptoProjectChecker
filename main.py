d69114b59c68758a441cdecaf90524bd8b8945fe
Denis Humen <dberkut18@gmail.com>
2025-03-28 13:14:11 +0200

    Refactor menu structure to include a sub-menu for MONAD actions and improve user navigation

--- main.py
+++ main.py
@@ -18,39 +18,60 @@ from modules.check_files import check_and_create_paths
 def menu():
     check_and_create_paths()
     try:
+        # основное меню
         while True:
             action = select(
                 "What do you want to do?",
                 choices=[
-                    Choice('💲 start stats MONAD', 'stats_monad'),
-                    Choice('🔍 gaszip monad faucet checker', 'gaszip_monad_faucet_checker'),
-                    Choice('🗑️ Clear wallet json data', 'clear_wallet_json_data'),
+                    Choice('💲 MONAD', 'monad'),
                     Choice('❌ Exit', 'exit')
                 ],
                 qmark='🛠️',
                 pointer='👉'
             ).ask()
 
+
             if action == 'exit':
                 break
-            elif action == 'stats_monad':
-                wallets, proxies = get_wallets_and_proxies()
-                reserv_proxies = get_reserv_proxies()
-                config = toml.load('config/general_config.toml')
-                num_threads = config.get('THRENDS', 10)
-                sleep_between_wallet = config.get('SLEEP_BEATWEEN_WALLET', [1, 3])
-                sleep_between_replace_proxy = config.get('SLEEP_BEATWEEN_REAPLECE_PROXY', [1, 3])
-                limit_replace_proxy = config.get('LIMIT_REPLACE_PROXY', 10)
-                
-                results = process_wallets(wallets, proxies, reserv_proxies, num_threads, sleep_between_wallet, sleep_between_replace_proxy, limit_replace_proxy)
-                process_results(results)
-                process_json_to_csv()
-            elif action == 'gaszip_monad_faucet_checker':
-                gaszip_monad_checker_process_wallets_from_csv()
-                gaszip_monad_checker_export_json_to_csv()
-            elif action == 'clear_wallet_json_data':
-                clear_wallet_json_data()
+            elif action == 'monad':
+
+                # подменю MONAD
+                while True:
+                    action = select(
+                        "What do you want to do?",
+                        choices=[
+                            Choice('💲 Start stats MONAD', 'stats_monad'),
+                            Choice('🔍 GasZip monad faucet checker', 'gaszip_monad_faucet_checker'),
+                            Choice('🗑️ Clear wallet json data | Удалит все созданные json после запроса', 'clear_wallet_json_data'),
+                            Choice('❌ Exit', 'exit')
+                        ],
+                        qmark='🛠️',
+                        pointer='👉'
+                    ).ask()
+
+                    if action == 'exit':
+                        break
+                    elif action == 'stats_monad':
+                        wallets, proxies = get_wallets_and_proxies()
+                        reserv_proxies = get_reserv_proxies()
+                        config = toml.load('config/general_config.toml')
+                        num_threads = config.get('THRENDS', 10)
+                        sleep_between_wallet = config.get('SLEEP_BEATWEEN_WALLET', [1, 3])
+                        sleep_between_replace_proxy = config.get('SLEEP_BEATWEEN_REAPLECE_PROXY', [1, 3])
+                        limit_replace_proxy = config.get('LIMIT_REPLACE_PROXY', 10)
+                        
+                        results = process_wallets(wallets, proxies, reserv_proxies, num_threads, sleep_between_wallet, sleep_between_replace_proxy, limit_replace_proxy)
+                        process_results(results)
+                        process_json_to_csv()
+                    elif action == 'gaszip_monad_faucet_checker':
+                        gaszip_monad_checker_process_wallets_from_csv()
+                        gaszip_monad_checker_export_json_to_csv()
+                    elif action == 'clear_wallet_json_data':
+                        clear_wallet_json_data()
         
+
+
+
 
     except Exception as e:
         print(f"Error: {str(e)}")
